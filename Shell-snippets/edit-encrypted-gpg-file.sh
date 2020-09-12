b244a1abd800c54fb43e5628ca1b9b5a1734bccd
cos <cosmineugendinu@gmail.com>
2020-09-13 01:59:30 +0300

    feat: Passwords with special characters

--- Shell-snippets/edit-encrypted-gpg-file.sh
+++ Shell-snippets/edit-encrypted-gpg-file.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 
-# decrypt CRYPT_FILE, pipe to vim, and encrypt againg whent type ':q!'
+# decrypt CRYPT_FILE, pipe to vim, and encrypt again when type ':q!'
 edit_crypt_file() {
         # first argument is a file encrypted with gpg
         CRYPT_FILE=$1
@@ -19,9 +19,16 @@ edit it with vim, then close the editor with ':q!'."
         vim_flags='- -n -u NONE --not-a-term'
 
         # vim command to run before exit with ':q!'
-        vim_autocmd=":autocmd VimLeave * :%! tee | gpg $gpg_flags --passphrase $pass -o $CRYPT_FILE -c"
+        vim_autocmd=":autocmd VimLeave * :%! tee | gpg $gpg_flags"
+        vim_autocmd+=' --passphrase '
+	# single quotes are necessary for passwords with special characters, like $
+	vim_autocmd+="'$pass'"
+        vim_autocmd+=" -o $CRYPT_FILE -c"
+	
+	echo ".........test............."
+	echo "$vim_autocmd"
 
-        gpg $gpg_flags --passphrase $pass -d $CRYPT_FILE | vim $vim_flags -c "$vim_autocmd"
+        gpg $gpg_flags --passphrase "$pass" -d $CRYPT_FILE | vim $vim_flags -c "$vim_autocmd"
 
         # restart agent in order to lose kept password
         gpgconf --kill gpg-agent
