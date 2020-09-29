81c81e733fd0c3845a0a834125c321ae5c4ef74d
CosTel <cosmineugendinu@gmail.com>
2020-09-29 18:55:43 +0300

    feat: Create {Set} from values

---
+++ JavaScript-snippets/set.js
@@ -0,0 +1,24 @@
+
+const assert = require('assert');
+
+/**
+ * creates {Set} from args
+ */
+const set = (...args) => args.reduce((set, arg)=>{
+  set.add(arg);
+  return set;
+}, new Set());
+
+
+const test = (set) => {
+  const expectedSet = new Set();
+  expectedSet.add('a');
+  const values = [1, 2, 3, 3, 0];
+  values.forEach(v => expectedSet.add(v));
+  assert.deepStrictEqual(set(...values), expectedSet);
+};
+test(set);
+
+
+
+
