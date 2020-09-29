4b99de09226b57ac5495c7bf3a3b6ee9bdaa3ea3
cos <cosmineugendinu@gmail.com>
2020-09-29 10:32:29 +0300

    feat: Fields Validator

---
+++ JavaScript-snippets/field_validator.js
@@ -0,0 +1,160 @@
+
+const getType = require('./get_type.js');
+const argumentsValidator = require('./arguments_validator.js');
+
+
+class FieldValidator{
+  constructor(){
+    this._fieldNames = new Map();
+    this._fieldIndexes = new Map();
+    this._validTypes = ['string', 'number', 'boolean', 'Date'];
+  }
+  /**
+   * @param {string} fieldName
+   * @param {number} fieldIndex
+   * @param {string} fieldType
+   * @param {*} [minValue]
+   * @param {*} [maxValue]
+   * @param {Set} [exactValues]
+   */
+  setField(fieldName, fieldIndex, fieldType, minValue, maxValue, exactValues){
+    
+    const [setArgTypes, validateArgs] = argumentsValidator();
+
+    // number of actual parameters
+    // last undefined actual parameters are excluded
+    const paramLength = arguments.length;
+    let lastArgIndex = paramLength;
+    while (--lastArgIndex > 0){
+      if (arguments[lastArgIndex] !== undefined) break;
+    }
+    const argsLength = lastArgIndex + 1;
+
+    if (argsLength < 3)
+      throw new TypeError('Invalid number of arguments');
+
+    if (argsLength === 3)
+      setArgTypes('string','number','string');
+    else if (argsLength === 4){
+      const minValueType = getType(minValue)==='null'?'null':fieldType;
+      setArgTypes('string','number','string', minValueType);
+    } else if (argsLength === 5){
+      const minValueType = getType(minValue)==='null'?'null':fieldType;
+      const maxValueType = getType(maxValue)==='null'?'null':fieldType;
+      setArgTypes('string','number','string', minValueType, maxValueType);
+    } else if (argsLength === 6)
+      setArgTypes('string','number','string', 'null', 'null', 'Set');
+
+
+    //setArgTypes('string','number','string',fieldType,fieldType,'Set');
+    validateArgs(...arguments);
+
+    const field = new Map();
+    if (this._fieldNames.has(fieldName))
+      throw new Error('fieldName already exists');
+    if (this._fieldIndexes.has(fieldIndex))
+      throw new Error('fieldIndex already exists'); 
+    field.set('name', fieldName);
+    field.set('index', fieldIndex);
+    field.set('type', fieldType);
+
+    if (minValue !== null && minValue !== undefined && exactValues === undefined)
+      field.set('minValue', minValue);
+    // set max value only if exactValues are not proviced
+    if (maxValue !== null && maxValue !== undefined && exactValues === undefined)
+      field.set('maxValue', maxValue);
+    // throw if type of exactValues is not the same as fieldType
+    if (exactValues !== undefined){
+      for (const exactVal of exactValues.keys()){
+        if (getType(exactVal) !== fieldType){
+          const err = new TypeError('Invalid field exactType');
+          err.fieldName = fieldName;
+          err.expectedType = fieldType;
+          err.currentType = getType(exactVal);
+          throw err;
+        }
+      }
+      field.set('exactValues', exactValues);
+    }
+
+    this._fieldNames.set(fieldName, field); 
+    // cannot be tow fields with same index;
+    this._fieldIndexes.set(fieldIndex, field);
+    return this;
+  }
+  
+  /**
+   * @param {string} fieldName
+   * @param {*} testValue
+   */
+  validate(fieldName, testValue){
+    const [setArgTypes, validateArgs] = argumentsValidator();
+    // validate arguments
+    setArgTypes('string', getType(testValue));
+    validateArgs(...arguments);
+
+    class ValueError extends Error{
+      constructor(msg){
+        super(msg);
+        this.name = 'ValueError';}}
+    class NotFoundError extends Error{
+      constructor(msg){
+        super(msg);
+        this.name = 'NotFoundError';}}
+
+    if ( ! this._fieldNames.has(fieldName))
+      throw new NotFoundError('Field not found');
+
+    const field = this._fieldNames.get(fieldName);
+
+    const valType = getType(testValue); 
+    // test for type
+    if (field.get('type') !== valType){
+      const err = new TypeError('Invalid Value Type');
+      err.fieldName = fieldName;
+      err.expectedType = field.get('type');
+      err.currentType = valType;
+      throw err;
+    }
+
+    // test for value
+    if (field.has('minValue')){
+      if (testValue < field.get('minValue')){
+        const err = new ValueError('Value less than minValue');
+        err.fieldName = fieldName;
+        err.expectedValue = `>= ${field.get('minValue')}`;
+        err.currentValue = testValue;
+        throw err;
+      }
+    }
+    if (field.has('maxValue')){
+      if (testValue > field.get('maxValue')){
+        const err = new ValueError('Value greater than maxValue');
+        err.fieldName = fieldName;
+        err.expectedValue = `<= ${field.get('maxValue')}`;
+        err.currentValue = testValue;
+        throw err;
+      }
+    }
+    if (field.has('exactValues')){
+      if ( ! field.get('exactValues').has(testValue)){
+        const err = new ValueError('Value not found in exactValues Set');
+        err.fieldName = fieldName;
+        err.currentValue = testValue;
+        throw err;
+      }
+    }
+    
+    return true;
+  }
+  getFieldByName(fieldName){
+    return this._fieldNames.get(fieldName);
+  }
+  getFieldByIndex(fieldIndex){
+    return this._fieldIndexes.get(fieldIndex);
+  }
+}
+
+module.exports = FieldValidator;
+
+
--- JavaScript-snippets/test/test.js
+++ JavaScript-snippets/test/test.js
@@ -4,7 +4,7 @@ const assert = require('assert');
 
 const getType = require('../get_type.js');
 const argumentsValidator = require('../arguments_validator.js');
-
+const FieldValidator = require('../field_validator.js');
 
 
 const tests = new Map();
@@ -73,5 +73,167 @@ tests.set('argumentsValidator', () => {
     });
 });
 tests.get('argumentsValidator')();
+
+
+tests.set('FieldValidator', () => {
+
+  const exactIntValues = new Set();
+  const exactStrValues = new Set();
+  exactIntValues.add(3); exactIntValues.add(4);
+  exactStrValues.add('c'); exactStrValues.add('d');
+  class CustomType{};
+
+  const testArguments = {
+    setField:{
+      correctArgs: [
+        ['nums',0,'number'],
+        ['nums',0,'number',0],
+        ['nums',0,'number',null],
+        ['nums',0,'number',null,2],
+        ['nums',0,'number',0,null],
+        ['nums',0,'number',0,2],
+        ['nums',0,'number',null,null],
+        ['nums',0,'number',null,null,exactIntValues]
+      ],
+      wrongNumOfArgs: [
+        [],
+        ['nums',0],
+        ['nums',0,'number',null,null,exactIntValues,'anotherOne']
+      ],
+      wrongTypeOfArgs: [
+        ['nums',0,'number','str'],
+        ['nums',0,'number',0,'str'],
+        ['nums',0,'number',null,'str'],
+        ['nums',0,'number','str',null],
+        ['nums',0,'number','str','str'],
+        ['nums',0,'number','str','str',exactIntValues],
+        ['nums',0,'number',null,'str',exactIntValues],
+        ['nums',0,'number','str',null,exactIntValues],
+        ['nums',0,'number',null,null,exactStrValues],
+      ],
+    },
+    validate:{
+      correctArgs: [
+        ['ints', 3],
+        ['objs', {}],
+        ['customs', new CustomType()],
+      ],
+      wrongNumOfArgs: [
+        ['ints'],
+        ['objs', {}, 'one more'],
+      ],
+      wrongTypeOfArgs: [
+        [3,2],
+        [{},2],
+      ],
+    },
+  };
+
+  // test correct arguments
+  for (const args of testArguments.setField.correctArgs){
+    assert.doesNotThrow(()=>{
+      new FieldValidator().setField(...args); 
+    })
+  }
+  // test wrong number of arguments
+  for (const args of testArguments.setField.wrongNumOfArgs){
+    assert.throws(()=>{
+      new FieldValidator().setField(...args);
+    },{name:'TypeError'});
+  }
+  // test wront types of arguments
+  for (const args of testArguments.setField.wrongTypeOfArgs){
+    assert.throws(()=>{
+      new FieldValidator().setField(...args);
+    },{name:'TypeError'});
+  }
+  // test correct arguments
+  for (const args of testArguments.validate.correctArgs){
+    assert.doesNotThrow(()=>{
+      new FieldValidator()
+        .setField(args[0],1,getType(args[1]))
+        .validate(...args); 
+    })
+  }
+  // test wrong number of arguments
+  for (const args of testArguments.validate.wrongNumOfArgs){
+    assert.throws(()=>{
+      new FieldValidator()
+        .setField(args[0],1,getType(args[1]))
+        .validate(...args);
+    },{name:'TypeError'});
+  }
+  // test wront types of arguments
+  for (const args of testArguments.validate.wrongTypeOfArgs){
+    assert.throws(()=>{
+      new FieldValidator()
+        .setField('fn',0,'number')
+        .validate(...args);
+    },{name:'TypeError'});
+  }
+  // test same name field
+  assert.throws(()=>{
+    new FieldValidator().setField('name',0,'number').setField('name',1,'number');
+  },{name:'Error',message:'fieldName already exists'});
+  // test form same index
+  assert.throws(()=>{
+    new FieldValidator().setField('name1',0,'number').setField('name2',0,'number');
+  },{name:'Error',message:'fieldIndex already exists'});
+
+  const exactDateValues = new Set();
+  const date1 = new Date(), date2 = new Date(), date3 = new Date();
+  date2.setDate(date1.getDate()+1); date3.setDate(date2.getDate()+1);
+  exactDateValues.add(date2); exactDateValues.add(date3);
+  const allTypesExcept_undefined = [null,true,'str',[],{},new Map(),new Set(),new Date()];
+  const allTypesExcept_null = [undefined,true,'str',[],{},new Map(),new Set(),new Date()];
+  const allTypesExcept_bool = [undefined,null,'str',[],{},new Map(),new Set(),new Date()];
+  const allTypesExcept_str = [undefined,null,true,1,[],{},new Map(),new Set(),new Date()];
+  const allTypesExcept_int = [undefined,null,true,'str',[],{},new Map(),new Set(),new Date()];
+  const allTypesExcept_date = [undefined,null,true,'str',[],{},new Map(),new Set()];
+  // test validate for return ant throw with different inputs
+  // for these expects to return true
+  const validator = new FieldValidator()
+  let caseNum = -1;
+  for (const [fieldDescription, correctValues, wrongValues, wrongValueTypes] of [
+    [['num',0,'number'],[-1,0,1,1.2,10000],[],allTypesExcept_int],
+    [['num',0,'number',0],[0,1,2,10000],[-1,-10000],allTypesExcept_int],
+    [['num',0,'number',null,2],[-10000,-1,0,1,2],[3,4],allTypesExcept_int],
+    [['num',0,'number',0,2],[0,1,2],[-1,3],allTypesExcept_int],
+    [['num',0,'number',0,null],[0,1,2,10000],[-1,-2],allTypesExcept_int],
+    [['num',0,'number',null,null,exactIntValues],[3,4],[-1,0,2,5],allTypesExcept_int],
+    [['date',0,'Date'],[date1,date2,date3,new Date()],[],allTypesExcept_date],
+    [['date',0,'Date',date2],[date2,date3],[date1],allTypesExcept_date],
+    [['date',0,'Date',date1,date2],[date1,date2],[date3],allTypesExcept_date],
+    [['date',0,'Date',null,date2],[date1,date2],[date3],allTypesExcept_date],
+    [['date',0,'Date',date2,null],[date2,date3],[date1],allTypesExcept_date],
+    [['date',0,'Date',null,null,exactDateValues],[date2,date3],[date1],allTypesExcept_date],
+    [['str',0,'string'],['str','a','b','c'],[],allTypesExcept_str],
+    [['str',0,'string',null,null],['str','a','b','c'],[],allTypesExcept_str],
+    [['str',0,'string','c'],['c','d'],['a','b'],allTypesExcept_str],
+    [['str',0,'string','b','d'],['b','c','d'],['a','e'],allTypesExcept_str],
+    [['str',0,'string',null,'d'],['b','c','d'],['e','f'],allTypesExcept_str],
+    [['str',0,'string','c',null],['c','d','e'],['a','b'],allTypesExcept_str],
+    [['str',0,'string',null,null,exactStrValues],['c','d'],['a','b','e'],allTypesExcept_str],
+  ]){
+    ++caseNum;
+    fieldDescription[0] += caseNum;
+    fieldDescription[1] = caseNum;
+    validator.setField(...fieldDescription);
+    const fieldName = fieldDescription[0];
+    correctValues.map(testValue => {
+      assert.strictEqual(validator.validate(fieldName, testValue),true);
+    });
+    wrongValues.map(testValue =>{
+      assert.throws(()=>{validator.validate(fieldName, testValue)},{name:'ValueError'},
+      `field description ${fieldDescription}, wrong test value is ${testValue}`);
+    });
+    wrongValueTypes.map(testValue =>{
+      assert.throws(()=>{validator.validate(fieldName, testValue)},{name:'TypeError'},
+      `field description ${fieldDescription}, wrong test value type is ${testValue}`);
+    });
+  }
+});
+tests.get('FieldValidator')();
+
 
 
