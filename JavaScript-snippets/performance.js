6a31e5574795538e0734298135e599fa3aacfb7f
CosTel <cosmineugendinu@gmail.com>
2020-09-30 10:41:28 +0300

    feat: Lookup speed test

---
+++ JavaScript-snippets/performance.js
@@ -0,0 +1,95 @@
+
+
+
+function setCreation(values, times){
+  const start = Date.now();
+
+  let count = 0;
+  while(++count < times){
+    const s = new Set();
+    for (const val of values)
+      s.add(val);
+  }
+
+  return Date.now() - start;
+}
+
+function arrayCreation(values, times){
+  const start = Date.now();
+
+  let count = 0;
+  while(++count < times){
+    const a = [];
+    for (const val of values)
+      a.push(val);
+  }
+
+  return Date.now() - start;
+}
+
+function objectCreation(values, times){
+  const start = Date.now();
+
+  let count = 0;
+  while(++count < times){
+    const o = {};
+    for (const val of values)
+      o[val] = val;
+  }
+
+  return Date.now() - start;
+}
+
+function setLookup(values, lookfor){
+  const start = Date.now();
+  
+  const s = new Set();
+  for (const val of values)
+    s.add(val);
+  const found = s.has(lookfor);
+
+  return Date.now() - start;
+}
+
+function arrayLookup(values, lookfor){
+  const start = Date.now();
+  
+  const a = [];
+  for (const val of values)
+    a.push(val);
+  const found = a.includes(lookfor);
+
+  return Date.now() - start;
+}
+
+function objectLookup(values, lookfor){
+  const start = Date.now();
+  
+  const o = {};
+  for (const val of values)
+    o[val] = val;
+  const found = o[lookfor];
+
+  return Date.now() - start;
+}
+
+
+// few values
+const few = ['number','string','object'];
+// lots of values
+const aLot = [];
+for (let i=0; i < 1000000; i++)
+  aLot.push('string'+i);
+
+const times = 100000;
+console.log(times,'first set creation done in',setCreation(few,times)); 
+console.log();
+console.log(times,'array  creation done in',arrayCreation(few,times)); 
+console.log(times,'set    creation done in',setCreation(few,times)); 
+console.log(times,'object creation done in',objectCreation(few,times)); 
+console.log();
+console.log('set    lookup done in',setLookup(aLot, aLot[aLot.length-1])); 
+console.log('array  lookup done in',arrayLookup(aLot, aLot[aLot.length-1])); 
+console.log('object lookup done in',objectLookup(aLot,aLot[aLot.length-1])); 
+console.log();
+
