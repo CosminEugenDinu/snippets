e23b1a159f532a29591a09ac962bba9d0c355b44
cos <cosmineugendinu@gmail.com>
2020-10-04 15:59:29 +0300

    feat: Array lookup values in inverse order

--- JavaScript-snippets/performance.js
+++ JavaScript-snippets/performance.js
@@ -1,168 +1,206 @@
 
 
 
-function setCreation(values, times){
+function setCreation(times){
   const start = Date.now();
-
   let count = 0;
   while(++count < times){
     const s = new Set();
-    for (const val of values)
-      s.add(val);
   }
-
-  return Date.now() - start;
+  const time = Date.now() - start;
+  console.log(`${times} new Set() created  in ${time}`);
 }
 
-function mapCreation(values, times){
+function mapCreation(times){
   const start = Date.now();
-
   let count = 0;
   while(++count < times){
     const m = new Map();
-    for (const val of values)
-      m.set(val, val);
   }
-
-  return Date.now() - start;
+  const time = Date.now() - start;
+  console.log(`${times} new Map() created  in ${time}`);
 }
 
-function arrayCreation(values, times){
+function arrayCreation(times){
   const start = Date.now();
-
   let count = 0;
   while(++count < times){
     const a = [];
-    for (const val of values)
-      a.push(val);
   }
-
-  return Date.now() - start;
+  const time = Date.now() - start;
+  console.log(`${times} array [] created  in ${time}`);
 }
 
-function objectCreation(values, times){
+function objectCreation(times){
   const start = Date.now();
-
   let count = 0;
   while(++count < times){
     const o = {};
-    for (const val of values)
-      o[val] = val;
   }
-
-  return Date.now() - start;
+  const time = Date.now() - start;
+  console.log(`${times} object {} created  in ${time}`);
 }
 
 function setLookup(values, lookfor){
-  let start = Date.now(); 
   const s = new Set();
+  let start = Date.now(); 
   for (const val of values)
     s.add(val);
-  console.log(values.length+' values added on set in '+(Date.now()-start));
+  console.log(values.length+' values added on Set in '+(Date.now()-start));
 
   start = Date.now();
   const found = s.has(lookfor);
+  console.log(`Set lookup for one value in ${Date.now() - start}`);
 
-  return Date.now() - start;
+  start = Date.now();
+  for (const val of values){
+    const found = s.has(val);
+  }
+  console.log(`Set lookup for ${values.length} values in ${Date.now() - start}`);
 }
 
 function mapLookup(values, lookfor){
-  let start = Date.now(); 
   const m = new Map();
+  let start = Date.now(); 
   for (const val of values)
     m.set(val, val);
-  console.log(values.length+' values set on map in '+(Date.now()-start));
+  console.log(values.length+' values added on Map in '+(Date.now()-start));
 
   start = Date.now();
   const found = m.has(lookfor);
+  console.log(`Map lookup for one value in ${Date.now() - start}`);
 
-  return Date.now() - start;
+  start = Date.now();
+  for (const val of values){
+    const found = m.has(val);
+  }
+  console.log(`Map lookup for ${values.length} values in ${Date.now() - start}`);
 }
 
-function arrayLookup(values, lookfor){
-  let start = Date.now();
+function arrayLookupByIncludes(values, lookfor){
   const a = [];
+  let start = Date.now();
   for (const val of values)
     a.push(val);
-  console.log(values.length+' pushed on array in '+(Date.now()-start));
+  console.log(values.length+' values added on Array in '+(Date.now()-start));
 
   start = Date.now();
   const found = a.includes(lookfor);
+  console.log(`Array "includes" lookup for one value in ${Date.now() - start}`);
 
-  return Date.now() - start;
+  start = Date.now();
+  for (const val of values){
+    const found = a.includes(val);
+  }
+  console.log(`Array "includes" lookup for ${values.length} values in ${Date.now() - start}`);
+  
+  const fromIndex = 10000;
+  const toIndex = 0;
+  start = Date.now();
+  for (let i=fromIndex; i>toIndex; i--){
+    const found = a.includes(values[i]);
+  }
+  console.log(`Array "includes" inverse lookup for ${fromIndex-toIndex} values in ${Date.now() - start}`);
 }
 
 function arrayLookupByIndexOf(values, lookfor){
-  let start = Date.now() 
   const a = [];
+  let start = Date.now() 
   for (const val of values)
     a.push(val);
-  console.log(values.length+' pushed on array in '+(Date.now()-start));
+  console.log(values.length+' values added on Array in '+(Date.now()-start));
 
   start = Date.now();
-  const foundIndex = a.indexOf(lookfor);
+  const found = a.indexOf(lookfor);
+  console.log(`Array "indexOf" lookup for one value in ${Date.now() - start}`);
 
-  return Date.now() - start;
+  start = Date.now();
+  for (const val of values){
+    const found = a.indexOf(val);
+  }
+  console.log(`Array "indexOf" lookup for ${values.length} values in ${Date.now() - start}`);
+
+  const fromIndex = 10000;
+  const toIndex = 0;
+  start = Date.now();
+  for (let i=fromIndex; i>toIndex; i--){
+    const found = a.indexOf(values[i]);
+  }
+  console.log(`Array "indexOf" inverse lookup for ${fromIndex-toIndex} values in ${Date.now() - start}`);
 }
 
 function arrayLookupByIndex(values, index){
-  let start = Date.now(); 
   const a = [];
+  let start = Date.now(); 
   for (const val of values)
     a.push(val);
-  console.log(values.length+' pushed on array in '+(Date.now()-start));
+  console.log(values.length+' values added on Array in '+(Date.now()-start));
 
   start = Date.now();
-  const found = a[index];
+  const found = a[values.length-2];
+  console.log(`Array "[i]" lookup for one value in ${Date.now() - start}`);
 
-  return Date.now() - start;
+  start = Date.now();
+  let i = 0;
+  for (const val of values){
+    const found = a[i++];
+  }
+  console.log(`Array "[i]" lookup for ${values.length} values in ${Date.now() - start}`);
 }
 
 function objectLookup(values, lookfor){
-  
   const o = {};
+  let start = Date.now(); 
   for (const val of values)
     o[val] = val;
+  console.log(values.length+' values added on Object in '+(Date.now()-start));
 
-  const start = Date.now();
+  start = Date.now();
   const found = o[lookfor];
+  console.log(`Object "[prop]" lookup for one value in ${Date.now() - start}`);
 
-  return Date.now() - start;
+  start = Date.now();
+  for (const val of values){
+    const found = o[val];
+  }
+  console.log(`Object "[prop]" lookup for ${values.length} values in ${Date.now() - start}`);
 }
 
 
 // few values
 const few = ['number','string','object'];
 // lots of values
-const longString = String.fromCharCode(...Array(100).fill(35).map((e,i)=>e+i));
+const longString = String.fromCharCode(...Array(500).fill(0).map((e,i)=>e+i));
 const aLot = [];
-for (let i=0; i < 1000000; i++)
+for (let i=0; i < 10000000; i++)
   aLot.push(longString+i);
   //aLot.push(1000+i);
 
-const times = 100000;
-console.log(times,'first set creation done in',setCreation(few,times)); 
+const times = 10000000;
+setCreation(times); 
 console.log();
-console.log(times,'array  creation done in',arrayCreation(few,times)); 
-console.log(times,'array  creation done in',arrayCreation(few,times)); 
-console.log(times,'set    creation done in',setCreation(few,times)); 
-console.log(times,'map    creation done in',mapCreation(few,times)); 
-console.log(times,'object creation done in',objectCreation(few,times)); 
+arrayCreation(times); 
+arrayCreation(times); 
+setCreation(times); 
+mapCreation(times); 
+objectCreation(times);
 console.log();
-console.log('set    "has" lookup done in',
-  setLookup(aLot, aLot[aLot.length-1])); 
-console.log('map    "has" lookup done in',
-  mapLookup(aLot, aLot[aLot.length-1])); 
-console.log('array  "includes" lookup done in',
-  arrayLookup(aLot, aLot[aLot.length-1])); 
-console.log('array  "indexOf" lookup done in',
-  arrayLookupByIndexOf(aLot, aLot[aLot.length-100])); 
-console.log('array  "includes" lookup done in',
-  arrayLookup(aLot, aLot[aLot.length-1])); 
-console.log('array  "[i]" lookup done in',
-  arrayLookupByIndex(aLot, aLot.length-100)); 
-//console.log('object "[prop]" lookup done in',
-  //objectLookup(aLot,aLot[aLot.length-1])); 
+setLookup(aLot, aLot[aLot.length-1]); 
+console.log();
+console.log();
+setLookup(aLot, aLot[aLot.length-1]); 
+console.log();
+mapLookup(aLot, aLot[aLot.length-1]); 
+console.log();
+setLookup(aLot, aLot[aLot.length-1]); 
+console.log();
+arrayLookupByIncludes(aLot, aLot[aLot.length-1]); 
+console.log();
+arrayLookupByIndexOf(aLot, aLot[aLot.length-100]);
+console.log();
+arrayLookupByIndex(aLot, aLot.length-100); 
+console.log();
+//objectLookup(aLot,aLot[aLot.length-1]); 
 console.log();
 
 
