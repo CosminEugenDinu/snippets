564d0574a8d80eee24b172871e64a4c3294ba6f5
cos <cosmineugendinu@gmail.com>
2020-11-04 20:41:39 +0200

    style: Indent

--- Shell-snippets/password-manager/edit-encrypted-gpg-file.sh
+++ Shell-snippets/password-manager/edit-encrypted-gpg-file.sh
@@ -3,47 +3,47 @@
 
 # decrypt CRYPT_FILE, pipe to vim, and encrypt again when type ':q!'
 edit_crypt_file() {
-        # first argument is a file encrypted with gpg
-        CRYPT_FILE=$1
-	if [ -z $1 ];then
-		printf 'First argument must be a encrypted.gpg file!\n'
-		exit 2
-	fi
-        echo "Enter your gpg encrypted file passphrase,
-edit it with vim, then close the editor with ':q!'."
+    # first argument is a file encrypted with gpg
+    CRYPT_FILE=$1
+    if [ -z $1 ];then
+            printf 'First argument must be a encrypted.gpg file!\n'
+            exit 2
+    fi
+    echo "Enter your gpg encrypted file passphrase,
+  edit it with vim, then close the editor with ':q!'."
 
-	# check for vim editor
-	if ! type vim;then
-		printf "Please install vim and try again!\n"
-		exit 1
-	fi
+    # check for vim editor
+    if ! type vim;then
+            printf "Please install vim and try again!\n"
+            exit 1
+    fi
 
-        # get password user input
-        local pass
-        read -sp "Password:" pass
+    # get password user input
+    local pass
+    read -sp "Password:" pass
 
-        gpg_flags='--batch --yes'
-        vim_flags='- -n -u NONE --not-a-term'
+    gpg_flags='--batch --yes'
+    vim_flags='- -n -u NONE --not-a-term'
 
-        # vim command to run before exit with ':q!'
-        vim_autocmd=":autocmd VimLeave * :%! tee | gpg $gpg_flags"
-        vim_autocmd+=' --passphrase '
-	# single quotes are necessary for passwords with special characters, like $
-	# then append backslash in from of % signs in password to escape vim autocmd special chars
-	vim_autocmd+="'${pass//\%/\\%}'"
-        vim_autocmd+=" -o $CRYPT_FILE -c"
+    # vim command to run before exit with ':q!'
+    vim_autocmd=":autocmd VimLeave * :%! tee | gpg $gpg_flags"
+    vim_autocmd+=' --passphrase '
+    # single quotes are necessary for passwords with special characters, like $
+    # then append backslash in from of % signs in password to escape vim autocmd special chars
+    vim_autocmd+="'${pass//\%/\\%}'"
+    vim_autocmd+=" -o $CRYPT_FILE -c"
 
-	decrypted=$(gpg $gpg_flags --passphrase "$pass" -d $CRYPT_FILE)
-	if [ $? = 0 ];then
-		printf "$decrypted" | vim $vim_flags -c "$vim_autocmd"
-	else
-		printf "Probably wrong password...\n"
-		exit 1
-	fi
+    decrypted=$(gpg $gpg_flags --passphrase "$pass" -d $CRYPT_FILE)
+    if [ $? = 0 ];then
+            printf "$decrypted" | vim $vim_flags -c "$vim_autocmd"
+    else
+            printf "Probably wrong password...\n"
+            exit 1
+    fi
 
-        # restart agent in order to lose kept password
-        gpgconf --kill gpg-agent
-        unset pass
+    # restart agent in order to lose kept password
+    gpgconf --kill gpg-agent
+    unset pass
 }
 
 edit_crypt_file $1
