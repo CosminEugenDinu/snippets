7d26ff48b823e487c40a1cbe0ec216ab5900e0b0
Cos <cosmineugendinu@gmail.com>
2021-10-11 08:57:29 +0300

    feat: Word Wrap Function

---
+++ JavaScript-snippets/wordWrap.js
@@ -0,0 +1,17 @@
+
+function wordWrap(str, lineLen) {
+  const wordRe = /\w+/g;
+  const newStr = [];
+  let wordMatch = wordRe.exec(str);
+  let row = 1;
+  while (wordMatch) {
+    if (wordRe.lastIndex < lineLen * row ) {
+      newStr.push(wordMatch[0]);
+    } else {
+      newStr.push('\n' + wordMatch[0]);
+      row ++;
+    }
+    wordMatch = wordRe.exec(str)
+  }
+  return newStr.join(' ');
+}
